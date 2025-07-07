48d7ca21075c9a55e3cea89a6dfb6a521b91cc80
Rafel Fernando <155946580+RafelFernando@users.noreply.github.com>
2025-07-07 09:43:54 +0700

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title> @yield('judul') | Laboratorium </title>
     <link rel="stylesheet" href="/bulma/css/bulma.min.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+
 </head>
 <body>
     <div class="container">
