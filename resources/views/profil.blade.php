7728bdb87465eb1b9cfb49fddb9d94719bc8fdef
Rafel Fernando <155946580+RafelFernando@users.noreply.github.com>
2025-07-07 09:45:02 +0700

    Update profil.blade.php

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -1,46 +1,41 @@
 @extends('layout')
-@section('judul', Profil')
+@section('judul', 'Profil')
 @section('konten')
-<section class="hero is-success">
-    <div class="hero-body">
-        <p class="title">Profil Mahasiswa</p>
-        <p class="subtitle">
 
-        Program Studi Sistem Informasi
-        </p>
+<section class="bg-green-500 text-white py-12">
+    <div class="container mx-auto px-4">
+        <h1 class="text-4xl font-bold">Profil Mahasiswa</h1>
+        <p class="text-xl mt-2">Program Studi Sistem Informasi</p>
     </div>
 </section>
 
-<section class="section has-background-primary-soft
-has-text-primary-soft-invert">
-    <div class="columns">
-        <div class="column is-one-third box">
-            <img class="image" src="/images/rafel.png">
+<section class="bg-green-50 py-12">
+    <div class="container mx-auto px-4 flex flex-col md:flex-row gap-8 items-start">
+        <div class="w-full md:w-1/3">
+            <img class="rounded-lg shadow-md w-full object-cover" src="/images/rafel.png" alt="Foto Profil Rafel">
         </div>
-        <div class="column">
-            <table class="table is-striped">
-        <tr>
-            <td>Nama</td>
-            <td>:</td>
-            <td> Rafel Fernando</td>
-
-        </tr>
-        <tr>
-
-            <td>Alamat</td>
-            <td>:</td>
-            <td> Sukoharjo</td>
-
-        </tr>
-        <tr>
-            <td>Kewarganegaraan</td>
-
-            <td>:</td>
-            <td> Indonesia</td>
-
-            </tr>
-        </table>
+        <div class="w-full md:w-2/3 bg-white rounded-lg shadow p-6">
+            <table class="w-full text-left border-collapse">
+                <tbody>
+                    <tr class="border-b">
+                        <td class="py-2 font-semibold text-gray-700 w-1/3">Nama</td>
+                        <td class="py-2 w-1">:</td>
+                        <td class="py-2 text-gray-800">Rafel Fernando</td>
+                    </tr>
+                    <tr class="border-b">
+                        <td class="py-2 font-semibold text-gray-700">Alamat</td>
+                        <td class="py-2">:</td>
+                        <td class="py-2 text-gray-800">Sukoharjo</td>
+                    </tr>
+                    <tr>
+                        <td class="py-2 font-semibold text-gray-700">Kewarganegaraan</td>
+                        <td class="py-2">:</td>
+                        <td class="py-2 text-gray-800">Indonesia</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </section>
+
 @endsection
