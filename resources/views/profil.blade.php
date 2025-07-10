97b1b2c4064903fc0b68a0db1d149ed8c567627e
Rafel Fernando <155946580+RafelFernando@users.noreply.github.com>
2025-07-10 17:58:50 +0700

    Update profil.blade.php

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -12,7 +12,7 @@
 <section class="bg-green-50 py-12">
     <div class="container mx-auto px-4 flex flex-col md:flex-row gap-8 items-start">
         <div class="w-full md:w-1/3">
-            <img class="rounded-lg shadow-md w-full object-cover" src="src="https://raw.githubusercontent.com/RafelFernando/Laboratorium-laravel/main/public/images/rafel.png"
+            <img class="rounded-lg shadow-md w-full object-cover" src="src="https://drive.google.com/uc?export=view&id=1xl5kFi7UoGzoKNi56FYcxVIIROTAVNEm"
 " alt="Foto Profil Rafel">
         </div>
         <div class="w-full md:w-2/3 bg-white rounded-lg shadow p-6">
